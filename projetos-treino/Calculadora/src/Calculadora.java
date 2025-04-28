e42087cbcd55684ed977ed947c91402b96be6813
Pedro Rocha <p.drohrocha23@gmail.com>
2025-04-28 15:00:12 -0300

    adicionei projetos-treino

---
+++ projetos-treino/Calculadora/src/Calculadora.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void menu(){
+        String menu;
+        menu =
+                """
+                \nSelecione o cálculo desejado:
+                1 - Soma
+                2 - Subtração
+                3 - Multiplicação
+                4 - Divisão
+                """;
+        System.out.println(menu);
+    }
+
+    public static float soma(float num1, float num2){
+        float soma;
+        soma = num1 + num2;
+        return soma;
+    }
+
+    public static float sub(float num1, float num2){
+        float sub;
+        sub = num1 - num2;
+        return sub;
+    }
+
+    public static float mult(float num1, float num2){
+        float mult;
+        mult = num1 * num2;
+        return mult;
+    }
+
+    public static float div(float num1, float num2){
+        float div;
+        div = num1 / num2;
+        return div;
+    }
+
+
+    public static void main(String[] args){
+
+        short opcao;
+        float num1, num2, resultado;
+        resultado = 0.0f;
+
+        menu();
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nEscolha uma opção:");
+        opcao = input.nextShort();
+
+        System.out.println("Digite o primeiro número: ");
+        num1 = input.nextFloat();
+
+        System.out.println("Digite o segundo número: ");
+        num2 = input.nextFloat();
+
+        if (opcao == 1){
+            resultado = soma(num1,num2);
+        }
+        if (opcao == 2){
+            resultado = sub(num1, num2);
+        }
+        if (opcao == 3){
+            resultado = mult(num1, num2);
+        }
+        if (opcao == 4){
+            resultado = div(num1, num2);
+        }
+
+        System.out.println("Resultado = " +  resultado);
+
+    }
+
+}
+
+
